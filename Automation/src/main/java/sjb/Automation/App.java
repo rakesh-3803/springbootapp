21f02a7c77ac6c8f4a038c7775d3cf11360c49b3
rakesh-3803 <rakeshlv618@gmail.com>
2025-07-11 03:44:08 +0530

    Automation(selenium)

---
+++ Automation/src/main/java/sjb/Automation/App.java
@@ -0,0 +1,30 @@
+package sjb.Automation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	WebDriverManager.chromedriver().setup();
+    	WebDriver driver=new ChromeDriver();
+    	driver.get("https://www.saucedemo.com/");
+    	WebElement ele=driver.findElement(By.id("user-name"));
+    	WebElement ele1=driver.findElement(By.id("password"));
+    	ele.sendKeys("standard_user");
+    	ele1.sendKeys("secret_sauce");
+    	WebElement btn=driver.findElement(By.name("login-button"));
+    	btn.submit();
+
+	
+    }
+}
